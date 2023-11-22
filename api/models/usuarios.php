62b2f94ee962d61a238b0d5ae8ebe735f6504ec7
JordyV23 <jordyvargas0123@gmail.com>
2023-11-22 15:44:25 -0600

    Creacion de modelos de rentas | usuarios | vehiculos

---
+++ api/models/usuarios.php
@@ -0,0 +1,48 @@
+<?php
+//Incluímos inicialmente la conexión a la base de datos
+require "../config/Conexion.php";
+
+
+class usuarios
+{
+    //Constructor de usuarios
+    public function __construct()
+    {
+    }
+
+    // Funcion para insertar usuarios
+    public function insertar($cedula, $nombre, $apellidos, $fechaNacimiento, $password, $email)
+    {
+        $sql = "INSERT INTO usuarios (cedula,nombre,apellidos,fechaNacimiento,password,email,rol)
+		VALUES ('$cedula', '$nombre','$apellidos','$fechaNacimiento','$password','$email','cliente')";
+        return EjecutarConsulta($sql);
+    }
+
+    // Funcion para listar todos los registros sobre usuarios
+    public function listar()
+    {
+        $sql = "SELECT cedula,nombre,apellidos,fechaNacimiento,password,email,rol FROM usuarios";
+        return EjecutarConsulta($sql);
+    }
+
+    // Funcion para editar informacion sobre usuarios
+    public function editar($cedula, $password, $email)
+    {
+        $sql = "UPDATE usuarios SET password='$password',email='$email' WHERE cedula = $cedula";
+        return EjecutarConsulta($sql);
+    }
+
+    // Funcion para buscar usuarios
+    public function buscar($cedula)
+    {
+        $sql = "SELECT cedula,nombre,apellidos,fechaNacimiento,password,email,rol FROM usuarios WHERE cedula = $cedula";
+        return EjecutarConsulta($sql);
+    }
+
+    // Funcion para eliminar usuarios
+    public function eliminar($cedula)
+    {
+        $sql = "DELETE FROM usuarios WHERE cedula = $cedula";
+        return EjecutarConsulta($sql);
+    }
+}
