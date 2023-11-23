9be13d7fa7dd7f7de854f17f075cf6976eda314a
JordyV23 <jordyvargas0123@gmail.com>
2023-11-22 19:53:10 -0600

    Reconfiguracion de variables

--- api/config/global.php
+++ api/config/global.php
@@ -1,15 +1,21 @@
 <?php 
+require '../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv->load();
+
+
 //Ip de la pc servidor de base de datos
-define("DB_HOST","localhost");
+define("DB_HOST",$_ENV['DBHOST']);
 
 //Nombre de la base de datos
-define("DB_NAME", "");
+define("DB_NAME", $_ENV['DBNAME']);
 
 //Usuario de la base de datos
-define("DB_USERNAME", "");
+define("DB_USERNAME", $_ENV['DBUSER']);
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", $_ENV['DBPASS']);
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
